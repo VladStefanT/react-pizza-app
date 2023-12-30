6a846f3b19b818e6e8e01cf78844f27b08196790
Tudorache Vlad <vladstefan8938@gmail.com>
2023-12-30 02:48:06 +0200

    Finished cart functionality using Redux

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,10 +17,10 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder="Search order number"
+        placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-900 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-44 sm:focus:w-72"
+        className="focus:placeholder:opacity-0 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-900 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-44 sm:focus:w-72"
       ></input>
     </form>
   );
